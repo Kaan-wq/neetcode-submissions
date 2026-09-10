ed71ab2419a42159821f1e34b401f79f5f5bbebd
Kaan <79455351+Kaan-wq@users.noreply.github.com>
2026-09-10 11:25:37 +0200

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        shash, thash = {}, {}
+        for (a, b) in zip(s, t):
+            shash[a] = shash.get(a, 0) + 1
+            thash[b] = thash.get(b, 0) + 1
+        return shash == thash
